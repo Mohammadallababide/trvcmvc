07550809e043702e870300e9ce09b808ef175d7e
Mohammadallababide <mohammad999alhos10@gmail.com>
2020-05-01 18:49:54 +0300

    Completate User Authentication and build Session for create account user and other for logedin user

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -3,6 +3,7 @@ class Users extends Controller
 {
   public function __construct()
   {
+    $this->userModel = $this->model('User');
   }
   public function Register()
   {
@@ -25,6 +26,11 @@ class Users extends Controller
       // Validate Email
       if (empty($data['email'])) {
         $data['email_err'] = 'Pleae enter email';
+      } else {
+        // Check email
+        if ($this->userModel->findUserByEmail($data['email'])) {
+          $data['email_err'] = 'Email is already having account';
+        }
       }
       // Validate Name
       if (empty($data['name'])) {
@@ -50,7 +56,14 @@ class Users extends Controller
       // Make sure errors are empty
       if (empty($data['email_err']) && empty($data['name_err']) && empty($data['password_err']) && empty($data['confirm_password_err'])) {
         // Validated
-        die('SUCCESS');
+        // Hash Password
+        $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+        if ($this->userModel->register($data)) {
+          flash('regester_success', 'You are regiestered you can login now');
+          redirect('users/login');
+        } else {
+          die('SomeThing Wrong');
+        }
       } else {
         // Load view with errors
         $this->view('users/register', $data);
@@ -96,10 +109,26 @@ class Users extends Controller
         $data['password_err'] = 'Pleae enter password';
       }
 
+      // Check for user/email
+      if ($this->userModel->findUserByEmail($data['email'])) {
+        // User found
+      } else {
+        // User not found
+        $data['email_err'] = 'No user found';
+      }
+
       // Make sure errors are empty
       if (empty($data['email_err']) && empty($data['password_err'])) {
         // Validated
-        die('SUCCESS');
+        // Check and set logged in user
+        $loggedInUser = $this->userModel->login($data['email'], $data['password']);
+        if ($loggedInUser) {
+        // Create Session 
+        $this->createUserSession($loggedInUser);
+        } else {
+          $data['password_err'] = 'password inorecte';
+          $this->view('users/login', $data);
+        }
       } else {
         // Load view with errors
         $this->view('users/login', $data);
@@ -120,5 +149,28 @@ class Users extends Controller
       // Load view
       $this->view('users/login', $data);
     }
+ }
+ public function createUserSession($user){
+  $_SESSION['user_id'] = $user->id;
+  $_SESSION['user_email'] = $user->email;
+  $_SESSION['user_name'] = $user->name;
+  redirect('pages/index');
+  }
+
+  public function logout(){
+    unset($_SESSION['user_id']);
+    unset($_SESSION['user_email']);
+    unset($_SESSION['user_name']);
+    session_destroy();
+    redirect('users/login');
+  }
+
+  public function isLoggedIn(){
+  if(isset($_SESSION['user_id']) ){
+    return true;
+  }
+  else{
+    return false;
+  }
   }
 }
--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -14,8 +14,12 @@
             <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">About</a>
           </li>
         </ul>
-        
         <ul class="navbar-nav ml-auto">
+        <?php if(isset($_SESSION['user_id'])):?>
+          <li class="nav-item">
+            <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout">Logout</a>
+          </li>
+          <?php else : ?>
           <li class="nav-item">
             <a class="nav-link" href="<?php echo URLROOT; ?>/users/register">Register</a>
           </li>
@@ -23,6 +27,7 @@
             <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">Login</a>
           </li>
         </ul>
+          <?php endif; ?>
       </div>
       </div>   
     </nav>
